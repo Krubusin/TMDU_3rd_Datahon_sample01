71651a99a6d3df788cd8c5b10ba229cfa3bbfc61
Krubusin <96191503+Krubusin@users.noreply.github.com>
2023-12-11 17:13:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import xgboost as xgb
 import numpy as np
 import pickle
 # モデルをファイルから読み込む
-with open('xgboost_model.pkl', 'rb') as f:
+#with open('xgboost_model.pkl', 'rb') as f:
+ #   loaded_model = pickle.load(f)
+
+
+with open('XGBoost_best_model_auc.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
+
 
 # ページのタイトルを設定
 st.title("The need to take ABG")
